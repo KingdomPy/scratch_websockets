972d3466270cd0b0dbd3b6cd6f4d0db7ecbd1ff0
KingdomPy <39922014+KingdomPy@users.noreply.github.com>
2019-02-22 22:16:37 +0000

    Add files via upload
    python server

---
+++ websocket server.py
@@ -0,0 +1,29 @@
+#Scratch websocket server
+
+#Server imports
+import asyncio
+import random
+import websockets
+
+#Data imports
+import json
+
+async def client(websocket, path):
+    print("Succesfully connected to scratch.")
+    receivedMessages = ["bob head", "simoan"]
+    while True:
+        data = await websocket.recv()
+        print("RECEIVED:", data)
+        if data == "%_fetch":
+            #If no messages
+            if receivedMessages == []:
+                await websocket.send("")
+                
+            else:
+                await websocket.send(json.dumps(receivedMessages))
+                receivedMessages = []
+
+start_server = websockets.serve(client, '127.0.0.1', 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
